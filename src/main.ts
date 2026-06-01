cc9d08b7b1089fb3217e45b37b272067105e3743
ZannGit <ahmad_fauzan_33rpl@student.smktelkom-mlg.sch.id>
2026-06-01 18:56:55 +0700

    oke

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.enableCors({
   origin: process.env.FRONTEND_URL
     ? process.env.FRONTEND_URL.split(',')
-    : 'http://localhost:3000',
+    : 'https://nyamnyamfrontend-production.up.railway.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
